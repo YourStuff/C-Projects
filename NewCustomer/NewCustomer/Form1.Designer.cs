85860b35d87344fe63151362057bcbf50cb08313
ijaafari <iijaafari@gmail.com>
2017-04-12 15:35:50 -0400

    New Customer project

---
+++ NewCustomer/NewCustomer/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace NewCustomer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.streetTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.zipTextBox = new System.Windows.Forms.TextBox();
+            this.bokButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.stateComboBox = new System.Windows.Forms.ComboBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(413, 24);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(162, 20);
+            this.firstNameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(335, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(335, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last Name";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(413, 65);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(162, 20);
+            this.lastNameTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Street";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // streetTextBox
+            // 
+            this.streetTextBox.Location = new System.Drawing.Point(413, 106);
+            this.streetTextBox.Name = "streetTextBox";
+            this.streetTextBox.Size = new System.Drawing.Size(162, 20);
+            this.streetTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(335, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "City";
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(413, 147);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(162, 20);
+            this.cityTextBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(335, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "State";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(497, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "ZIP";
+            // 
+            // zipTextBox
+            // 
+            this.zipTextBox.Location = new System.Drawing.Point(535, 188);
+            this.zipTextBox.Name = "zipTextBox";
+            this.zipTextBox.Size = new System.Drawing.Size(40, 20);
+            this.zipTextBox.TabIndex = 12;
+            this.zipTextBox.Text = "19702";
+            // 
+            // bokButton
+            // 
+            this.bokButton.Location = new System.Drawing.Point(413, 248);
+            this.bokButton.Name = "bokButton";
+            this.bokButton.Size = new System.Drawing.Size(75, 23);
+            this.bokButton.TabIndex = 14;
+            this.bokButton.Text = "OK";
+            this.bokButton.UseVisualStyleBackColor = true;
+            this.bokButton.Click += new System.EventHandler(this.bokButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(500, 248);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 15;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // stateComboBox
+            // 
+            this.stateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.stateComboBox.FormattingEnabled = true;
+            this.stateComboBox.Location = new System.Drawing.Point(413, 191);
+            this.stateComboBox.Name = "stateComboBox";
+            this.stateComboBox.Size = new System.Drawing.Size(48, 21);
+            this.stateComboBox.TabIndex = 17;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(366, 304);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(218, 37);
+            this.resultLabel.TabIndex = 18;
+            this.resultLabel.Text = "Result Label";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.resultLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.bokButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(613, 390);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.stateComboBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.bokButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.zipTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cityTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.streetTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.firstNameTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "New Customer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox streetTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox zipTextBox;
+        private System.Windows.Forms.Button bokButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ComboBox stateComboBox;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ NewCustomer/NewCustomer/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewCustomer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bokButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "New	Customer Created";
+            resultLabel.BackColor = Color.LightGreen;
+            resultLabel.Visible = true;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "Operation Canceled";
+            resultLabel.BackColor = Color.HotPink;
+            resultLabel.Visible = true;
+        }
+    }
+}
